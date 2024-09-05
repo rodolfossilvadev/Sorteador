d34c4b70a2951cb81f110a1cc68762e4ab694920
rodolfossilvadev <rodolfodev30@gmail.com>
2024-09-05 14:32:20 -0300

    inserindo alert

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ function generatenumber() {
     const min = Math.ceil(document.querySelector(".input-entre").value)
     const max = Math.floor(document.querySelector(".input-e").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
-    alert(result)
+    if (min >= max) {
+        alert("O valor mínimo tem que ser maior que o valor máximo")
+    } else {
+
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        alert(result)
+    }
 }
